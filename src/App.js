355220ef218c5cbb295d7791b63ca333bb739a1a
Harshvardhan <67060748+HarshVardhan22@users.noreply.github.com>
2021-08-22 03:11:20 +0530

    +implemented neoMOrphic UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styles from "./App.module.css";
+import data from "./data";
+// import { GrYoutube } from "react-icons/gr";
+import { FaStar } from "react-icons/fa";
+import Fade from "react-reveal/Fade";
 
-function App() {
+const App = () => {
+  const [index, setIndex] = useState(0);
+  const [btn1, setBtn1] = useState(true);
+  const [btn2, setBtn2] = useState(false);
+  const [btn3, setBtn3] = useState(false);
+
+  const handleClick1 = () => {
+    setIndex(0);
+    setBtn1(true);
+    setBtn2(false);
+    setBtn3(false);
+  };
+  const handleClick2 = () => {
+    setIndex(1);
+    setBtn1(false);
+    setBtn2(true);
+    setBtn3(false);
+  };
+  const handleClick3 = () => {
+    setIndex(2);
+    setBtn1(false);
+    setBtn2(false);
+    setBtn3(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.parent}>
+      <div className={styles.text}>
+        <Fade top>
+          <h1>Want to be a</h1>
+          <h3 style={{ fontSize: "78px", color: "crimson" }}>Software Dev?</h3>
+          <h2>Kick Start with</h2>
+          <h1>
+            {" "}
+            <span style={{ fontSize: "50px", color: "crimson" }}>
+              Recommendation{" "}
+            </span>
+            App
+          </h1>
+        </Fade>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.buttonRow}>
+          <Fade left>
+            <button
+              style={{
+                color: `${btn1 ? "#1f1f1f" : "whitesmoke"}`,
+                background: `${btn1 ? "#3fe878" : "#221e1c"}`,
+                boxShadow: `${
+                  btn1
+                    ? "inset 5px 5px 7px #32ba60,inset -5px -5px 7px #4cff90"
+                    : " 5px 5px 7px #94989a,-5px -5px 7px #ffffff"
+                }`,
+              }}
+              onClick={handleClick1}
+            >
+              Competitve Programming
+            </button>
+            <button
+              style={{
+                color: `${btn2 ? "#1f1f1f" : "whitesmoke"}`,
+                background: `${btn2 ? "#3fe878" : "#221e1c"}`,
+                boxShadow: `${
+                  btn2
+                    ? "inset 5px 5px 7px #32ba60,inset -5px -5px 7px #4cff90"
+                    : " 5px 5px 7px #94989a,-5px -5px 7px #ffffff"
+                }`,
+              }}
+              onClick={handleClick2}
+            >
+              Web Developement
+            </button>
+            <button
+              style={{
+                color: `${btn3 ? "#1f1f1f" : "whitesmoke"}`,
+                background: `${btn3 ? "#3fe878" : "#221e1c"}`,
+                boxShadow: `${
+                  btn3
+                    ? "inset 5px 5px 7px #32ba60,inset -5px -5px 7px #4cff90"
+                    : " 5px 5px 7px #94989a,-5px -5px 7px #ffffff"
+                }`,
+              }}
+              onClick={handleClick3}
+            >
+              Career Guidance
+            </button>
+          </Fade>
+        </div>
+        <div className={styles.miniCont}>
+          {data[`${index}`].map((item, key) => {
+            return (
+              <Fade right>
+                <div className={styles.card}>
+                  <h2>{item.title}</h2>
+                  <div className={styles.link}>
+                    <FaStar  />
+                    <h3>{item.rating}</h3>
+                    <button className={styles.goto}>
+                      <a href={item.link}>Link</a>
+                    </button>
+                  </div>
+                </div>
+              </Fade>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,84 @@
+const data = [
+  //for competitve programming
+  [
+    {
+      title: "HackerRank",
+      rating: "4.8",
+      link: "https://www.hackerrank.com/"
+    },
+    {
+      title: "InterviewBit",
+      rating: "4.4",
+      link: "https://www.interviewbit.com/"
+    },
+    // {
+    //   title: "LeetCode",
+    //   rating: "4.3",
+    //   link: "https://leetcode.com/"
+    // },
+    {
+      title: "GeeksForGeeks",
+      rating: "4.2",
+      link: "https://www.geeksforgeeks.org/"
+    },
+    {
+      title: "BinarySearch",
+      rating: "3.9",
+      link: "https://binarysearch.com/"
+    },
+  ],
+  // web developement Youtube channel
+
+  [
+    {
+      title: "Akshay Saini",
+      rating: "5",
+      link: "https://www.youtube.com/channel/UC3N9i_KvKZYP4F84FPIzgPQ"
+    },
+    {
+      title: "WebDev Simplified",
+      rating: "4.2",
+      link: "https://www.youtube.com/channel/UCFbNIlppjAuEX4znoulh0Cw"
+    },
+    {
+      title: "FreeCodeCamp",
+      rating: "4.1",
+      link: "https://www.youtube.com/channel/UC8butISFwT-Wl7EV0hUK0BQ"
+    },
+    {
+      title: "Clever Programmer",
+      rating: "3.9",
+      link: "https://www.youtube.com/channel/UCqrILQNl5Ed9Dz6CGMyvMTQ"
+    },
+    // {
+    //   title: "Dev Ed",
+    //   rating: "3.9",
+    //   link: "https://www.youtube.com/channel/UClb90NQQcskPUGDIXsQEz5Q"
+    // },
+  ],
+  //Youtube for career guidance
+  [
+    {
+      title: "Tanay Pratap",
+      rating: "5",
+      link: "https://www.youtube.com/channel/UCNFmBuclxQPe57orKiQbyfA"
+    },
+    {
+      title: "Love Babbar",
+      rating: "5",
+      link: "https://www.youtube.com/channel/UCQHLxxBFrbfdrk1jF0moTpw"
+    },
+    {
+      title: "Striver",
+      rating: "4.7",
+      link: "https://www.youtube.com/channel/UCvEKHATlVq84hm1jduTYm8g"
+    },
+    {
+      title: "Anuj Bhaiya",
+      rating: "4.5",
+      link: "https://www.youtube.com/channel/UC58_wzhvJta3hDSPvRLDAqg"
+    },
+  ],
+];
+
+export default data;
